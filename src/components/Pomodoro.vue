01bc8cc691c3f9d2e8dc2575dadcd34dc129adc3
soregashi-27 <tomo.soul.15@gmail.com>
2021-11-23 23:42:33 +0900

    tabの作成

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,13 +1,29 @@
 <template>
-<div>Clock</div>
+<v-card class="mt-8">
+  <v-tabs v-model="timerType">
+    <v-tab
+      v-for="tab in tabTitles"
+      :key="tab"
+    >
+    {{ tab }}
+    </v-tab>
+  </v-tabs>
+</v-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return{
+      timerType: 0,
+      tabTitles: ['Pomodoro', 'Short Break', 'Long Break']
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="sass" scoped>
+.v-card 
+  width: 400px
 
 </style>
